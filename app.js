79e843a50acf7e3d475f4440a9709a37c6b8815e
Ankit Verma <syncing284@gmail.com>
2025-08-29 18:17:24 +0530

    add routes

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 import express from 'express'
 import { PORT } from './config/env.js'
+import userRouter from './routes/user.routes.js'
+import authRouter from './routes/auth.routes.js'
+import subscriptionRouter from './routes/subscription.routes.js'
 
 const app = express()
+
+// Good practice to keep naming as plural
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/users', userRouter)
+app.use('/api/v1/subscriptions', subscriptionRouter)
 
 app.get('/', (req, res) => {
   res.send('Hello World')
---
+++ routes/subscription.routes.js
@@ -0,0 +1,53 @@
+import { Router } from "express";
+
+const subscriptionRouter = Router()
+
+subscriptionRouter.get('/', (req, res) => {
+  res.send({
+    message: 'GET all subscriptions'
+  })
+})
+
+subscriptionRouter.get('/:id', (req, res) => {
+  res.send({
+    message: 'GET subscription details'
+  })
+})
+
+subscriptionRouter.post('/', (req, res) => {
+  res.send({
+    message: 'CREATE subscription'
+  })
+})
+
+subscriptionRouter.put('/:id', (req, res) => {
+  res.send({
+    message: 'UPDATE subscription'
+  })
+})
+
+subscriptionRouter.delete('/:id', (req, res) => {
+  res.send({
+    message: 'DELETE subscription'
+  })
+})
+
+subscriptionRouter.get('/user/:id', (req, res) => {
+  res.send({
+    message: 'GET all user subscriptions'
+  })
+})
+
+subscriptionRouter.put('/:id', (req, res) => {
+  res.send({
+    message: 'CANCEL subscription'
+  })
+})
+
+subscriptionRouter.get('/upcoming-renewals', (req, res) => {
+  res.send({
+    message: 'GET upcoming renewals'
+  })
+})
+
+export default subscriptionRouter
